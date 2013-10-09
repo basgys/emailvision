5e949bc5a1f93ead4213af64ec6d7a43347954be
Bastien <basgys@gmail.com>
2013-10-10 00:33:33 +0200

    [Spec] Add some specs

--- Gemfile
+++ Gemfile
@@ -2,3 +2,14 @@
 source "http://rubygems.org"
 
 gemspec
+
+group :development, :test do
+  gem 'guard'
+end
+
+group :test do  
+  gem 'guard-rspec'
+  gem 'rspec'
+  gem 'vcr'
+  gem 'webmock', '< 1.12'
+end
---
+++ spec/lib/emailvision/api_spec.rb
@@ -0,0 +1,89 @@
+# encoding: utf-8
+require 'spec_helper'
+
+describe Emailvision::Api do
+
+  subject do 
+    Emailvision::Api.new(
+      server_name: 'emvapi.emv3.com',
+      login: 'api_login',
+      password: 'api_password',
+      key: 'api_key',
+      endpoint: 'apimember'
+    )
+  end
+
+  it "opens connection successfuly" do
+    VCR.use_cassette('opens connection successfuly') do
+      subject.open_connection
+      expect(subject.connected?).to be(true)
+    end    
+  end
+
+  it "closes connection successfuly" do    
+    VCR.use_cassette('closes connection successfuly') do      
+      subject.token = '111token111'
+      expect(subject.close_connection).to eq("connection closed")
+      expect(subject.connected?).to be(false)
+    end
+  end
+
+  context 'Member' do
+
+    before(:each) do
+      subject.stub(:token) { '111token111' }
+    end
+
+    it "retrieves member by email successfuly" do
+      VCR.use_cassette('retrieves member by email successfuly') do
+        subject.token = '111token111'
+        result = subject.get.member.get_member_by_email(uri: ['basgys@gmail.com']).call        
+        expect(result).to eq({
+          "members"=>{
+            "member"=>{
+              "attributes"=>{
+                "entry"=>[
+                  {"key"=>"MEMBER_ID", "value"=>"1"}, 
+                  {"key"=>"EMAIL", "value"=>"basgys@gmail.com"}, 
+                  {"key"=>"ANOTHER_FIELD", "value"=>"another value"}
+                ]
+              }
+            }
+          }, 
+          "responseStatus"=>"success"
+        })
+      end
+    end
+
+    it "retrieves member by id successfuly" do
+      VCR.use_cassette('retrieves member by id successfuly') do
+        subject.token = '111token111'
+        result = subject.get.member.get_member_by_id(id: 1).call
+        expect(result).to eq({
+          "members"=>{
+            "member"=>{
+              "attributes"=>{
+                "entry"=>[
+                  {"key"=>"MEMBER_ID", "value"=>"1"}, 
+                  {"key"=>"EMAIL", "value"=>"basgys@gmail.com"}, 
+                  {"key"=>"ANOTHER_FIELD", "value"=>"another value"}
+                ]
+              }
+            }
+          }, 
+          "responseStatus"=>"success"
+        })
+      end 
+    end
+
+    it "cannot find a member with this email" do
+      VCR.use_cassette('cannot find a member with this email') do
+        subject.token = '111token111'
+        result = subject.get.member.get_member_by_email(uri: ['fake@ema.il']).call
+        expect(result).to eq({"members"=>nil, "responseStatus"=>"success"})
+      end
+    end
+
+  end
+
+end
