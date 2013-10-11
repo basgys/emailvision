588cebd8dd8639dbef5461b531ff5a49e332b369
basgys <basgys@gmail.com>
2013-10-11 11:18:11 +0200

    [Spec] Add specs to Api

--- spec/lib/emailvision/api_spec.rb
+++ spec/lib/emailvision/api_spec.rb
@@ -17,15 +17,52 @@ describe Emailvision::Api do
     VCR.use_cassette('opens connection successfuly') do
       subject.open_connection
       expect(subject.connected?).to be(true)
-    end    
+    end
   end
 
-  it "closes connection successfuly" do    
-    VCR.use_cassette('closes connection successfuly') do      
+  it "closes connection successfuly" do
+    VCR.use_cassette('closes connection successfuly') do
       subject.token = '111token111'
       expect(subject.close_connection).to eq("connection closed")
       expect(subject.connected?).to be(false)
     end
+  end
+
+  it "invalidates token properly" do
+    subject.token = '111token111'
+    expect(subject.invalidate_token!).to eq(nil)
+    expect(subject.connected?).to be(false)
+  end
+
+  it "deconnects when the endpoint is changed" do
+    subject.token = '111token111'
+    VCR.use_cassette('deconnects when the endpoint is changed') do
+      subject.endpoint = 'apiccmd'
+      expect(subject.connected?).to be(false)
+    end
+  end
+
+  it "returns base_uri" do
+    expect(subject.base_uri).to eq("http://#{subject.server_name}/#{subject.endpoint}/services/rest/")
+  end
+
+  context 'Relation' do
+
+    let(:relation) { Emailvision::Relation }
+
+    before do
+      Emailvision::Relation.stub(new: relation)
+    end    
+
+    [:get, :post].each do |verb|
+
+      it "returns a relation when #{verb} is called" do
+        expect(relation).to receive(:new).with(subject, verb)
+        expect(subject.send(verb)).to be_a(relation.class)
+      end
+
+    end
+
   end
 
   context 'Member' do
@@ -33,7 +70,7 @@ describe Emailvision::Api do
     it "retrieves member by email successfuly" do
       VCR.use_cassette('retrieves member by email successfuly') do
         subject.token = '111token111'
-        result = subject.get.member.get_member_by_email(uri: ['basgys@gmail.com']).call        
+        result = subject.get.member.get_member_by_email(uri: ['basgys@gmail.com']).call
         expect(result).to eq({
           "members"=>{
             "member"=>{
